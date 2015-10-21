282f43245a0f9cfda0565941b099e9270a660d1d
Angel Jimenez <ajimenezescobar@gmail.com>
2015-10-21 18:59:43 -0500

    Create CustomLoginResult class

---
+++ Service Project/XamarinPushDemoService/CustomLoginResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinPushDemoService
+{
+    public class CustomLoginResult
+    {
+        public string UserId { get; set; }
+        public string MobileServiceAuthenticationToken { get; set; }
+    }
+}
