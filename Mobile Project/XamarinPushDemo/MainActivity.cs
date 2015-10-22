ba9bae47aa8897ba9fa421105d968f85b29d7c36
Angel Jimenez <ajimenezescobar@gmail.com>
2015-10-22 01:37:28 -0500

    Create Main Activity

---
+++ Mobile Project/XamarinPushDemo/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinPushDemo
+{	
+	[Activity (MainLauncher = true, 
+		Icon="@drawable/ic_launcher", Label="MainActivity",
+		Theme="@style/AppTheme")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Login);
+		}
+	}
+}
+
